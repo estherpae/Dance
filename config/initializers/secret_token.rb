2449e0535153cd2f7cf50cdf4ed887ff00af536e
Your name <you@email.com>
2013-08-03 22:19:30 -0500

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dance::Application.config.secret_key_base = '2d19fcb885d2005cc0fe2f952075cfcb2d6cb751cc77e74847de5fda75b8e731070095e3e5e7bbc72c14d439d67f46eb7ec5c69c6dd0743cf3446a9fa4fc445a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Dance::Application.config.session_store :cookie_store, key: '_dance_session'
