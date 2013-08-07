7d7524158fa0a2df1fc13a8634e9ba9ab179db6c
Your name <you@email.com>
2013-08-06 22:47:43 -0500

    Added list of classes in Instructors and studios

--- app/models/dance_class.rb
+++ app/models/dance_class.rb
@@ -2,5 +2,6 @@ class DanceClass < ActiveRecord::Base
   belongs_to :instructor
   belongs_to :studio
 
+
   validates :instructor_id, :studio_id, :when, :time, :cost, :presence => true
 end
--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,6 +1,7 @@
 class Instructor < ActiveRecord::Base
   has_many :dance_classes
 
+
   validates :name, :biography, :uniqueness => true
   validates :name, :style, :biography, :presence => true
 end
